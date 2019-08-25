1e3e2fe82da45ab7280bd4935f3ae3288b2cc06f
LeilaDev <leiladevops@gmail.com>
2019-08-25 15:14:23 -0500

    added if clause and terraform version

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -12,14 +12,22 @@ pipeline{
                 '''
             }
         }
-        stage("Download Terraform"){
+       tage("Download Terraform"){
             steps{
                 ws("tmp/"){
-                    sh "pwd"
-                    sh "wget https://releases.hashicorp.com/terraform/0.12.7/terraform_0.12.7_linux_amd64.zip"
-                    sh "unzip terraform_0.12.7_linux_amd64.zip"
-                    sh "sudo mv terraform /bin"
-                    sh "terraform version"
+                    script {
+                        def exists = fileExists 'terraform_0.12.7_linux_amd64.zip'
+                        if (exists) {
+                            sh "unzip -o terraform_0.12.7_linux_amd64.zip"
+                            sh "mv terraform /bin"
+                            sh "terraform version"
+                        } else {
+                            sh "wget https://releases.hashicorp.com/terraform/0.12.7/terraform_0.12.7_linux_amd64.zip"
+                            sh "unzip -o terraform_0.12.7_linux_amd64.zip"
+                            sh "mv terraform /bin"
+                            sh "terraform version"
+                        }
+                    }
                 }
             }
         }
