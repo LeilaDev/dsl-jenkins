5f0d9a4e6d93d398377d174017995d129ce4beb8
LeilaDev <leiladevops@gmail.com>
2019-08-25 14:58:11 -0500

    added wget

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -16,7 +16,7 @@ pipeline{
             steps{
                 ws("tmp/"){
                     sh "pwd"
-                    sh "https://releases.hashicorp.com/terraform/0.12.7/terraform_0.12.7_linux_amd64.zip"
+                    sh "wget https://releases.hashicorp.com/terraform/0.12.7/terraform_0.12.7_linux_amd64.zip"
 
                 }
             }
